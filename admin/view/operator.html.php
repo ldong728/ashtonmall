01fa2f577e7930ece22995a1b5632f4b614a9f7d
ldong728 <ldong728@gmail.com>
2016-03-18 12:06:28 +0800

    commit from t400
    操作员管理界面制作中

--- admin/view/operator.html.php
+++ admin/view/operator.html.php
@@ -2,3 +2,22 @@
 $opList=$GLOBALS['opList'];
 $pmsList=$GLOBALS['pmsList'];
 ?>
+
+<div class="op-container">
+    <div class="op-block">
+        <div class="op-name">
+            操作员
+        </div>
+        <div class="op-psw">
+            密码
+        </div>
+        <div class="op-pms">
+            <?php foreach($pmsList as $row):?>
+            <div class="op-pms-block">
+                <?php echo $row['name']?>
+            </div>
+            <?php endforeach ?>
+        </div>
+    </div>
+
+</div>
